a8a454ef943838487576e703c2d3ce973139206e
ltcmichelli <ltcmichelli@link.cuhk.edu.hk>
2024-06-04 22:46:47 +0800

    Title

---
+++ src/component/Title.tsx
@@ -0,0 +1,14 @@
+import './Title.css';
+
+function Title() {
+    return (
+        <div className="Parallax-cover">
+
+        <h1 className="Headline">Hello, I am Michelle Li</h1>
+        <div className='linkedIn'></div>
+        {/* https://portfolio-n4sn.vercel.app/#about */}
+        </div>
+  );
+}
+
+export default Title;
