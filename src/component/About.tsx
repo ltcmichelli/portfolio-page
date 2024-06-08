596599c28a58d451086b61a6d3b32c1a49fc0eae
ltcmichelli <ltcmichelli@link.cuhk.edu.hk>
2024-06-08 22:49:09 +0800

    fix about page responsive

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -8,54 +8,60 @@ function About() {
         <div className="Parallax-cover2">
             <div className="About-content">
                 <h1 className="About-title">About Me</h1>
-                <Container className='Education'>
+                <Container className='About-subtitle'>
                     <Row>
-                        <Col>
+                        <Col xs={3} sm={3}>
+                            <div className="vl"></div>
                             <h2>Education</h2>
                         </Col>
-                        <Col xs={9}>
+                        <Col xs={12} sm={9}>
                             <Row>
-                                <h2>Master of Science Degree in Computer Science</h2>
+                                <h4>Master of Science Degree in Computer Science</h4>
                             </Row>
-                            <Row>
-                                <small> The Chinese University of Hong Kong (Sep 2021 - Jun 2023)</small>
+                            <Row className='Row-samll'>
+                                <div> The Chinese University of Hong Kong (Sep 2021 - Jun 2023)</div>
                             </Row>
+                            <hr/>
                             <Row>
-                                <h2>Bachelor of System Engineering and Engineering Management</h2>
+                                <h4>Bachelor of System Engineering and Engineering Management</h4>
                             </Row>
-                            <Row>
-                                <small> The Chinese University of Hong Kong (Sep 2015 - Jun 2019)</small>
+                            <Row className='Row-samll'>
+                                <div> The Chinese University of Hong Kong (Sep 2015 - Jun 2019)</div>
                             </Row>
                         </Col>
                     </Row> 
                 </Container>
                 <hr></hr>
-                <Container className='Education'>
+                <Container className='About-subtitle'>
                     <Row>
-                        <Col>
+                        <Col xs={12} sm={3}>
+                            <div className="vl"></div>
                             <h2>Work</h2>
                         </Col>
-                        <Col xs={9}>
+                        <Col xs={12} sm={9}>
                             <Row>
-                                <h2>Technical Analyst</h2>
+                                <h4>Technical Analyst</h4>
                             </Row>
-                            <Row>
-                                <small> Overseas Container Line Limited (Nov 2021 - Present)</small>
+                            <Row className='Row-samll'>
+                                <div> Overseas Container Line Limited (Nov 2021 - Present)</div>
                             </Row>
+                            <hr/>
                             <Row>
-                                <h2>Assistant Technical Analyst</h2>
+                                <h4>Assistant Technical Analyst</h4>
                             </Row>
-                            <Row>
-                                <small> Overseas Container Line Limited (Aug 2019 - Nov 2021)</small>
+                            <Row className='Row-samll'>
+                                <div> Overseas Container Line Limited (Aug 2019 - Nov 2021)</div>
                             </Row>
+                            <hr/>
                             <Row>
-                                <h2>IT Summer Intern</h2>
+                                <h4>IT Summer Intern</h4>
                             </Row>
-                            <Row>
-                                <small> Cathay Pacific Airways Limited (Jul 2018 - Aug 2018)</small>
+                            <Row className='Row-samll'>
+                                <div> Cathay Pacific Airways Limited (Jul 2018 - Aug 2018)</div>
                             </Row>
                         </Col>
                     </Row> 
+                    <br></br>
                 </Container>
             </div>
         </div>
