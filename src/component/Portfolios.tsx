cb6eb3ec8342ccd6b04983e72ca7d539e5eacd54
ltcmichelli <ltcmichelli@link.cuhk.edu.hk>
2024-06-08 23:57:31 +0800

    fix favicon, css

--- src/component/Portfolios.tsx
+++ src/component/Portfolios.tsx
@@ -3,6 +3,13 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import image01 from '../img/01-mockup.png';
+import image02 from '../img/02-mockup.png';
+import image03 from '../img/03-mockup.png';
+import image04 from '../img/04-design.png';
+import image05 from '../img/05-design.png';
+import image06 from '../img/06-design.png';
+import image07 from '../img/07-design.png';
 
 function Portfolios() {
     return (
@@ -11,9 +18,9 @@ function Portfolios() {
                 <h1 className="Portfolios-title">Portfolios</h1>
                 <Container>
                     <Row className="Portfolios-row">
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/01-mockup.png" />
+                                <Card.Img variant="top" src={image01}/>
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>Catch Me if You Can</b></p>
@@ -25,9 +32,9 @@ function Portfolios() {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/02-mockup.png" />
+                                <Card.Img variant="top" src={image02} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>FoodStep</b></p>
@@ -40,9 +47,9 @@ function Portfolios() {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/03-mockup.png" />
+                                <Card.Img variant="top" src={image03} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>Easy Park</b></p>
@@ -58,9 +65,9 @@ function Portfolios() {
                         </Col>
                     </Row>
                     <Row className="Portfolios-row">
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/04-design.png" />
+                                <Card.Img variant="top" src={image04} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>United College Drama Club Card design</b></p>
@@ -71,9 +78,9 @@ function Portfolios() {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/05-design.png" />
+                                <Card.Img variant="top" src={image05} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>T shirt design of 2016 CUHK drama competition</b></p>
@@ -84,9 +91,9 @@ function Portfolios() {
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/06-design.png" />
+                                <Card.Img variant="top" src={image06} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>T shirt design of UCDC annual performance 2017</b></p>
@@ -99,9 +106,9 @@ function Portfolios() {
                         </Col>
                     </Row>
                     <Row className="Portfolios-row">
-                        <Col xs={12} sm={4} className='Portfolios-col'>
+                        <Col xs={12} sm={6} md={4} className='Portfolios-col'>
                             <Card className="Portfolios-card">
-                                <Card.Img variant="top" src="/portfolios/07-design.png" />
+                                <Card.Img variant="top" src={image07} />
                                 <Card.Body>
                                 <Card.Text>
                                 <p><b>Medicare (UI design)</b></p>
